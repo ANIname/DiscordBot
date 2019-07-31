7d3a05251a2d12a8102baa228cabf168040a9c6e
kiidii <kiidii.aniname@gmail.com>
2019-07-31 18:10:38 +0300

    Check Token existence

--- client.js
+++ client.js
@@ -7,6 +7,17 @@ const token = require('./config/token')[process.env.NODE_ENV || 'development'];
 const client = new Discord.Client();
 
 (async() => {
+  if (!token) {
+    return console.error(
+      'Not found BOT TOKEN. Create file "config/token.json". With the following content:\n\n',
+
+      JSON.stringify({
+        "development": "PASTE BOT TOKEN HERE",
+        "production": "PASTE BOT TOKEN HERE"
+      }, null, 2)
+    );
+  }
+
   try {
     await client.login(token);
   } catch (error) {
