715d1489efa974f03a907864c128d44e9326ee6d
Vadim <kiidii.aniname@gmail.com>
2019-08-03 18:50:43 +0300

    Added the ability to add an undefined amount of experience

--- modules/experience.js
+++ modules/experience.js
@@ -4,11 +4,19 @@ const User = require('../models/User');
 const {experience} = require('../config');
 
 module.exports = {
-  give: (id, forWhat) => {
-    User.findOneAndUpdate({id}, {$inc: {xp: experience.give[forWhat]}}, {upsert:true}).exec()
+  give: (id, xp, forWhat) => {
+    if (xp) {
+      return User.findOneAndUpdate({id}, {$inc: {xp}}, {upsert: true}).exec();
+    }
+
+    User.findOneAndUpdate({id}, {$inc: {xp: experience.give[forWhat]}}, {upsert: true}).exec();
   },
   
-  take: (id, forWhat) => {
-    User.findOneAndUpdate({id}, {$inc: {xp: -experience.take[forWhat]}}, {upsert:true}).exec()
+  take: (id, xp, forWhat) => {
+    if (xp) {
+      return User.findOneAndUpdate({id}, {$inc: {xp: -xp}}, {upsert: true}).exec();
+    }
+
+    User.findOneAndUpdate({id}, {$inc: {xp: -experience.take[forWhat]}}, {upsert: true}).exec();
   }
 }
