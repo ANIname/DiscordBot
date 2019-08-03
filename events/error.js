92fc49394990eba80316895d1360d60a6925ad6a
Vadim <kiidii.aniname@gmail.com>
2019-08-03 16:27:21 +0300

    Add mongoose

--- events/error.js
+++ events/error.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const client = require('../client');
+const db = require('../db');
 const Log = require('../modules/Logger')
 
 client.on('error', error => new Log('error', error));
+
+db.on('error', error => new Log('error', error));
--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const client = require('../client');
+const db = require('../db');
 
-client.on('ready', async () => {
-  console.info(`${client.user.username} ready to work!`);
+client.on('ready', () => {
+  console.info(`${client.user.username} bot ready to work!`);
+});
+
+db.once('open', () => {
+  console.info(`Connected to ${db.db.databaseName} database`);
 });
