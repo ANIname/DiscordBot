9dc99c23eb9599b48b6a8a28fa27ae80f3bf1a14
Vadim <kiidii.aniname@gmail.com>
2019-08-03 18:54:12 +0300

    Add error handler

--- modules/experience.js
+++ modules/experience.js
@@ -1,22 +1,35 @@
 'use strict';
 
 const User = require('../models/User');
+const Log = require('../modules/Logger');
 const {experience} = require('../config');
 
 module.exports = {
   give: (id, xp, forWhat) => {
-    if (xp) {
-      return User.findOneAndUpdate({id}, {$inc: {xp}}, {upsert: true}).exec();
-    }
+    try {
+      if (xp) {
+        return User.findOneAndUpdate({id}, {$inc: {xp}}, {upsert: true}).exec();
+      }
+  
+      User.findOneAndUpdate({id}, {$inc: {xp: experience.give[forWhat]}}, {upsert: true}).exec();
+    } catch(error) {
+      error.footer = 'Method: experience.give';
 
-    User.findOneAndUpdate({id}, {$inc: {xp: experience.give[forWhat]}}, {upsert: true}).exec();
+      new Log('error', error);
+    }
   },
   
   take: (id, xp, forWhat) => {
-    if (xp) {
-      return User.findOneAndUpdate({id}, {$inc: {xp: -xp}}, {upsert: true}).exec();
-    }
+    try {
+      if (xp) {
+        return User.findOneAndUpdate({id}, {$inc: {xp: -xp}}, {upsert: true}).exec();
+      }
+  
+      User.findOneAndUpdate({id}, {$inc: {xp: -experience.take[forWhat]}}, {upsert: true}).exec();
+    } catch(error) {
+      error.footer = 'Method: experience.take';
 
-    User.findOneAndUpdate({id}, {$inc: {xp: -experience.take[forWhat]}}, {upsert: true}).exec();
+      new Log('error', error);
+    }
   }
 }
