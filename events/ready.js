67e789b2a3577e04ca18f1b2533ac3ff4a03c1bb
kiidii <kiidii.aniname@gmail.com>
2019-08-19 04:10:39 +0300

    Add simple activity

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,6 @@
-module.exports = (client) => console.info(`${client.user.username} is ready to work`);
+module.exports = (client) => {
+  console.info(`${client.user.username} is ready to work`);
+
+  // noinspection JSIgnoredPromiseFromCall
+  client.user.setActivity('YouTube', { type: 'WATCHING' });
+};
