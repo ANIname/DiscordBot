69be138a7eb4ba7619bb804a31c3bab75396d02f
KiiDii <kiidii.aniname@gmail.com>
2020-12-24 00:46:48 +0200

    commands cleanUp

--- src/commands/messages-bulk-delete.js
+++ src/commands/messages-bulk-delete.js
@@ -1,34 +1,13 @@
-const find   = require('lodash/find');
 const client = require('../client');
 
-/**
- *
- * @param {Object} interaction               - An interaction is the base "thing" that is sent when a user invokes a command, and is the same for Slash Commands and other future interaction types
- * @param {String} interaction.id            - id of the interaction
- * @param {(1|2) } interaction.type          - the type of interaction
- * @param {Object} interaction.data          - the command data payload
- * @param {String} interaction.guild_id      - the guild it was sent from
- * @param {String} interaction.channel_id    - the channel it was sent from
- * @param {Object} interaction.member        - guild member data for the invoking user
- * @param {String} interaction.token         - a continuation token for responding to the interaction
- * @param {Number} interaction.version       - read-only property, always 1
- *
- * @returns {
- *   Promise<{
- *     data: { content: string },
- *     type: number
- *   }
- * >}
- */
 async function messagesBulkDeleteCommand(interaction) {
-  const numberOfMessagesOption = find(interaction.data.options, (option) => option.name === 'number-of-messages');
-  const numberOfMessages       = numberOfMessagesOption.value;
-  const guildId                = interaction.guild_id;
-  const channelId              = interaction.channel_id;
-  const memberId               = interaction.member.user.id;
-  const guild                  = await client.guilds.fetch(guildId);
-  const member                 = await guild.members.fetch(memberId);
-  const channel                = await guild.channels.cache.get(channelId);
+  const numberOfMessages = interaction.data.options;
+  const guildId          = interaction.guild_id;
+  const channelId        = interaction.channel_id;
+  const memberId         = interaction.member.user.id;
+  const guild            = await client.guilds.fetch(guildId);
+  const member           = await guild.members.fetch(memberId);
+  const channel          = await guild.channels.cache.get(channelId);
 
   if (!member.hasPermission('MANAGE_MESSAGES')) {
     return {
--- src/events/INTERACTION_CREATE.js
+++ src/events/INTERACTION_CREATE.js
@@ -1,8 +1,31 @@
+const forEach  = require('lodash/forEach');
 const client   = require('../client');
 const commands = require('../commands');
 
+/**
+ *
+ * @param {Object} interaction               - An interaction is the base "thing" that is sent when a user invokes a command, and is the same for Slash Commands and other future interaction types
+ * @param {String} interaction.id            - id of the interaction
+ * @param {(1|2) } interaction.type          - the type of interaction
+ * @param {Object} interaction.data          - the command data payload
+ * @param {String} interaction.guild_id      - the guild it was sent from
+ * @param {String} interaction.channel_id    - the channel it was sent from
+ * @param {Object} interaction.member        - guild member data for the invoking user
+ * @param {String} interaction.token         - a continuation token for responding to the interaction
+ * @param {Number} interaction.version       - read-only property, always 1
+ *
+ * @returns {
+ *   Promise<{
+ *     data: { content: string },
+ *     type: number
+ *   }
+ * >}
+ */
 client.ws.on('INTERACTION_CREATE', async (interaction) => {
-  const { name: commandName } = interaction.data
+  const commandName = interaction.data.name;
+
+  interaction.data.options = cleanUpCommandOptions(interaction.data.options);
+
 
   if (typeof commands[commandName] !== 'function') return;
 
@@ -10,3 +33,11 @@ client.ws.on('INTERACTION_CREATE', async (interaction) => {
 
   client.api.interactions(interaction.id, interaction.token).callback.post({ data: response });
 });
+
+function cleanUpCommandOptions(commandOptions) {
+  const cleanUppedCommandOptions = {};
+
+  forEach(commandOptions, (commandOption) => cleanUppedCommandOptions[commandOption.name] = commandOption.value);
+
+  return cleanUppedCommandOptions;
+}
